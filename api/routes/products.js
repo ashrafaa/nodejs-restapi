56589f9c71c181681b3688210b41b1abc3322d57
ashrafa <ashraf.a@spartadigital.co.uk>
2018-02-05 17:51:49 +0000

    Added schema validation; also better response message

--- api/routes/products.js
+++ api/routes/products.js
@@ -3,15 +3,31 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const Product = require('../models/product');
+const servername = 'http://localhost:3000/';
 
 // already been in products route
 router.get('/', (req, res, next) => {
     Product.find()
+        .select('name price _id')
         .exec()
         .then(docs => {
+            const response = {
+                count: docs.length,
+                products: docs.map(doc => {
+                    return {
+                        name: doc.name,
+                        price: doc.price,
+                        _id: doc._id,
+                        request: { // to add metadata
+                            type: 'GET',
+                            url: servername + 'products/' + doc._id
+                        }
+                    }
+                })
+            };
             console.log(docs);
             if (docs.length >= 0) {
-                res.status(200).json(docs);                
+                res.status(200).json(response);                
             } else {
                 res.status(404).json({
                     message: 'No entries found'
@@ -35,8 +51,16 @@ router.post('/', (req, res, next) => {
         .then(result => { // process as promise
             console.log(result);
             res.status(201).json({
-                message: 'Handling POST requests to /products',
-                createdProduct: product
+                message: 'Created product successfully',
+                createdProduct: {
+                    name: result.name,
+                    price: result.price,
+                    _id: result._id,
+                    request: {
+                        type: 'POST',
+                        url: servername + 'products/' + result._id
+                    }
+                }
             });        
         })
         .catch(err => {
@@ -51,11 +75,17 @@ router.post('/', (req, res, next) => {
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
     Product.findById(id)
+        .select('name price _id')
         .exec()
         .then(doc => {
-            console.log(doc);
             if (doc) {
-                res.status(200).json(doc);                
+                res.status(200).json({
+                    product: doc,
+                    request: {
+                        type: 'GET',
+                        url: servername + 'products'
+                    }
+                });                
             } else {
                 res.status(404).json({ message: 'No valid entry found for that id'})
             }
@@ -75,8 +105,13 @@ router.patch('/:productId', (req, res, next) => {
     Product.update({ _id: id }, { $set: updateOps })
         .exec()
         .then(result => {
-            console.log(result);
-            res.status(200).json(result);
+            res.status(200).json({
+                product: result,
+                request: {
+                    type: 'GET',
+                    url: servername + 'products/' + id 
+                }
+            });
         })
         .catch(err => {
             res.status(500).json({ error: err });
@@ -88,7 +123,9 @@ router.delete('/:productId', (req, res, next) => {
     Product.remove({ _id: id })
         .exec()
         .then(result => {
-            res.status(200).json(result);
+            res.status(200).json({
+                message: 'Product deleted'
+            });
         })
         .catch(err => {
             res.status(500).json({ error: err });
--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 mongoose.connect('mongodb://ashraf:ashraf@cluster0-shard-00-00-el9ts.mongodb.net:27017,cluster0-shard-00-01-el9ts.mongodb.net:27017,cluster0-shard-00-02-el9ts.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
+mongoose.Promise = global.Promise;
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false })); 
