5bd71d55042bde26f2cdab73677ccc2bfaf9e574
ashrafa <ashraf.a@spartadigital.co.uk>
2018-02-05 17:17:13 +0000

    Added MongoDB connection and use of mongoose; CRUD operation from remote database

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb://ashraf:ashraf@cluster0-shard-00-00-el9ts.mongodb.net:27017,cluster0-shard-00-01-el9ts.mongodb.net:27017,cluster0-shard-00-02-el9ts.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false })); 
