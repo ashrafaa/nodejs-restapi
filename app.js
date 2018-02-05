6589a1f017e82546aeb6d8855e7de76b9a28af14
ashrafa <ashraf.a@spartadigital.co.uk>
2018-02-05 15:47:57 +0000

    Added morgan & nodemon; also handling error due to invalid route call

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
+// Routes which should handle requests
 app.use('/products', productRoutes); // (path, handlers)
 app.use('/orders', orderRoutes);
+
+// Handle error for invalid routes 
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+// Handle all error thrown
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
